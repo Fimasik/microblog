002e61027c978f12310a08c5a449513929080f38
Dmitry <fimasik@gmail.com>
2018-05-30 18:43:16 +0300

    [ADD] form for send personal message function

--- app/main/routes.py
+++ app/main/routes.py
@@ -12,6 +12,9 @@ from app.main import bp
 
 from app.main.forms import SearchForm
 
+from app.main.forms import MessageForm
+from app.models import Message
+
 
 @bp.before_app_request
 def before_request():
@@ -157,3 +160,19 @@ def search():
 def user_popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user_popup.html', user=user)
+
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user,
+                      body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html', title=_('Send Message'),
+                           form=form, recipient=recipient)
